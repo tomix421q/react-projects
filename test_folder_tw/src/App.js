e765c4353402524184a67749f560f2f200313b74
tomix421q <zilka.tomas421@gmail.com>
2023-01-20 14:47:18 +0100

    pia

--- test_folder_tw/src/App.js
+++ test_folder_tw/src/App.js
@@ -1,27 +1,10 @@
 import React from 'react'
+import Navbar from './components/Navbar'
 
 const ExampleApp = () => {
   return (
-    <div className='bg-gray-200 min-h-screen'>
-      <header className='bg-blue-500 text-white p-4'>
-        <h1 className='text-xl font-bold'>Example App</h1>
-      </header>
-      <main className='container mx-auto p-4'>
-        <div className='grid grid-cols-3 gap-4'>
-          <div className='bg-white rounded-lg p-4'>
-            <p className='text-lg'>Column 1</p>
-          </div>
-          <div className='bg-white rounded-lg p-4'>
-            <p className='text-lg'>Column 2</p>
-          </div>
-          <div className='bg-white rounded-lg p-4'>
-            <p className='text-lg'>Column 3</p>
-          </div>
-        </div>
-      </main>
-      <footer className='bg-blue-500 text-white p-4'>
-        <p className='text-sm'>Copyright ©2022 Example App</p>
-      </footer>
+    <div>
+      <Navbar />
     </div>
   )
 }
--- test_folder_tw/src/Slot.js
+++
@@ -1,4 +0,0 @@
-const Slot = () => {
-  return <div className='bg-purple-200'>Slot</div>
-}
-export default Slot

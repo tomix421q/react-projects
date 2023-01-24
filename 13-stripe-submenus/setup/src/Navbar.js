fea795796d77c7ca28403d09bfa1b09b0c3ab646
tomix421q <zilka.tomas421@gmail.com>
2023-01-24 16:02:27 +0100

    ut

--- 13-stripe-submenus/setup/src/Navbar.js
+++ 13-stripe-submenus/setup/src/Navbar.js
@@ -5,6 +5,7 @@ import { useGlobalContext } from './context'
 
 const Navbar = () => {
   const { openSidebar, openSubmenu, closeSubmenu } = useGlobalContext()
+
   const displaySubmenu = (e) => {
     const page = e.target.textContent
     const tempBtn = e.target.getBoundingClientRect()

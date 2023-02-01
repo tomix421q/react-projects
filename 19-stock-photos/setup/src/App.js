b3731948b84506d64fc9a22842d07f9ff99a73d8
tomix421q <zilka.tomas421@gmail.com>
2023-02-01 21:21:09 +0100

    ST

--- 19-stock-photos/setup/src/App.js
+++ 19-stock-photos/setup/src/App.js
@@ -2,11 +2,79 @@ import React, { useState, useEffect } from 'react'
 import { FaSearch } from 'react-icons/fa'
 import Photo from './Photo'
 // const clientID = `?client_id=${process.env.REACT_APP_ACCESS_KEY}`
+const clientID = `?client_id=${process.env.REACT_APP_ACCESS_KEY}`
 const mainUrl = `https://api.unsplash.com/photos/`
 const searchUrl = `https://api.unsplash.com/search/photos/`
 
 function App() {
-  return <h2>stock photos starter</h2>
+  const [loading, setLoading] = useState(false)
+  const [photos, setPhotos] = useState([])
+
+  const fetchImages = async () => {
+    setLoading(true)
+    let url
+    url = `${mainUrl}${clientID}`
+    try {
+      const response = await fetch(url)
+      const data = await response.json()
+      setPhotos(data)
+      setLoading(false)
+    } catch (error) {
+      setLoading(false)
+    }
+  }
+
+  //
+  // USE EFFECTS
+  //
+  useEffect(() => {
+    fetchImages()
+  }, [])
+
+  useEffect(() => {
+    const event = window.addEventListener('scroll', () => {
+      // console.log(`innerHeight ${window.innerHeight}`)
+      // console.log(`scrollY ${window.scrollY}`)
+      // console.log(`body height ${document.body.scrollHeight}`)
+
+      if (
+        !loading &&
+        window.innerHeight + window.scrollY >= document.body.scrollHeight - 2
+      ) {
+        console.log('work')
+      }
+    })
+    return () => window.removeEventListener('scroll', event)
+  }, [])
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+  }
+
+  //
+  // RETURN
+  //
+
+  return (
+    <main>
+      <section className='search'>
+        <form className='search-form'>
+          <input type='text' placeholder='search' className='form-input' />
+          <button className='submit-btn' type='submit' onClick={handleSubmit}>
+            <FaSearch />
+          </button>
+        </form>
+      </section>
+      <section className='photos'>
+        <div className='photos-center'>
+          {photos.map((image, index) => {
+            return <Photo key={image.id} {...image} />
+          })}
+        </div>
+        {loading && <h2 className='loading'>Loading...</h2>}
+      </section>
+    </main>
+  )
 }
 
 export default App

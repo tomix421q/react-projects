bc9acb62bb14c250015192a7ea7713a4b8d38f49
tomix421q <zilka.tomas421@gmail.com>
2023-02-01 15:58:08 +0100

    st

--- 18-pagination/setup/src/App.js
+++ 18-pagination/setup/src/App.js
@@ -4,12 +4,16 @@ import Follower from './Follower'
 function App() {
   const { loading, data } = useFetch()
   const [page, setPage] = useState(0)
-  const [follower, setFollowers] = useState([])
+  const [followers, setFollowers] = useState([])
 
   useEffect(() => {
     if (loading) return
     setFollowers(data[page])
-  }, [loading])
+  }, [loading, page])
+
+  const handlePage = (index) => {
+    setPage(index)
+  }
 
   return (
     <main>
@@ -19,10 +23,25 @@ function App() {
       </div>
       <section className='followers'>
         <div className='container'>
-          {data.map((follower) => {
+          {followers.map((follower) => {
             return <Follower key={follower.id} {...follower} />
           })}
         </div>
+        {!loading && (
+          <div className='btn-container'>
+            {data.map((item, index) => {
+              return (
+                <button
+                  key={index}
+                  className={`page-btn ${index === page ? 'active-btn' : null}`}
+                  onClick={() => handlePage(index)}
+                >
+                  {index + 1}
+                </button>
+              )
+            })}
+          </div>
+        )}
       </section>
     </main>
   )
--- 18-pagination/setup/src/utils.js
+++ 18-pagination/setup/src/utils.js
@@ -1,6 +1,6 @@
 const paginate = (followers) => {
   const itemsPerPage = 9
-  const pages = Math.ceil(followers.lenght / itemsPerPage)
+  const pages = Math.ceil(followers.length / itemsPerPage)
 
   const newFollowers = Array.from({ length: pages }, (_, index) => {
     const start = index * itemsPerPage

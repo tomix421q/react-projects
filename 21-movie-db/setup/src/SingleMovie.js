f91eb29fb726501670e989909bbc89f0a4d9534e
tomix421q <zilka.tomas421@gmail.com>
2023-02-08 20:32:08 +0100

    st

--- 21-movie-db/setup/src/SingleMovie.js
+++ 21-movie-db/setup/src/SingleMovie.js
@@ -1,29 +1,11 @@
-import React, { useState, useEffect } from 'react'
+// import React from 'react'
 import { useParams, Link } from 'react-router-dom'
-import { API_ENDPOINT } from './context'
 import { url } from './Movies'
+import useFetch from './useFetch'
 
 const SingleMovie = () => {
   const { id } = useParams()
-  const [movie, setMovie] = useState({})
-  const [isLoading, setLoading] = useState(true)
-  const [error, setError] = useState({ show: false, msg: '' })
-
-  const fetchMovie = async (url) => {
-    const response = await fetch(url)
-    const data = await response.json()
-    if (data.Response === 'False') {
-      setError({ show: true, msg: data.error })
-      setLoading(false)
-    } else {
-      setMovie(data)
-      setLoading(false)
-    }
-  }
-
-  useEffect(() => {
-    fetchMovie(`${API_ENDPOINT}&i=${id}`)
-  }, [id])
+  const { isLoading, error, data: movie } = useFetch(`&i=${id}`)
 
   if (isLoading) {
     return <div className='loading'></div>
--- 21-movie-db/setup/src/index.js
+++ 21-movie-db/setup/src/index.js
@@ -1,10 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import { AppProvider } from './context';
-import { BrowserRouter as Router } from 'react-router-dom';
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import App from './App'
+import { AppProvider } from './context'
+import { BrowserRouter as Router } from 'react-router-dom'
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
     <AppProvider>
@@ -13,4 +14,4 @@ root.render(
       </Router>
     </AppProvider>
   </React.StrictMode>
-);
+)

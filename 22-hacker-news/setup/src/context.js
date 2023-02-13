58e81b5b29bcc8266d66871ac9ebb1cc33a2ef6f
tomix421q <zilka.tomas421@gmail.com>
2023-02-13 21:43:46 +0100

    pon

--- 22-hacker-news/setup/src/context.js
+++ 22-hacker-news/setup/src/context.js
@@ -38,12 +38,28 @@ const AppProvider = ({ children }) => {
     }
   }
 
+  const removeStory = (id) => {
+    dispatch({ type: REMOVE_STORY, payload: id })
+  }
+
+  const handleSearch = (query) => {
+    dispatch({ type: HANDLE_SEARCH, payload: query })
+  }
+
+  const handlePage = (value) => {
+    dispatch({ type: HANDLE_PAGE, payload: value })
+  }
+
   useEffect(() => {
     fetchStories(`${API_ENDPOINT}query=${state.query}&page=${state.page}`)
-  }, [])
+  }, [state.query, state.page])
 
   return (
-    <AppContext.Provider value={{ ...state }}>{children}</AppContext.Provider>
+    <AppContext.Provider
+      value={{ ...state, removeStory, handleSearch, handlePage }}
+    >
+      {children}
+    </AppContext.Provider>
   )
 }
 // make sure use
--- 22-hacker-news/setup/src/reducer.js
+++ 22-hacker-news/setup/src/reducer.js
@@ -20,6 +20,32 @@ const reducer = (state, action) => {
         nbPages: action.payload.nbPages,
       }
     //
+    case REMOVE_STORY:
+      return {
+        ...state,
+        hits: state.hits.filter((story) => story.objectID !== action.payload),
+      }
+    //
+    case HANDLE_SEARCH:
+      return { ...state, query: action.payload, page: 0 }
+    //
+    //
+    case HANDLE_PAGE:
+      if (action.payload === 'inc') {
+        let nextPage = state.page + 1
+        if (nextPage > state.nbPages - 1) {
+          nextPage = 0
+        }
+        return { ...state, page: nextPage }
+      }
+      if (action.payload === 'dec') {
+        let prevPage = state.page - 1
+        if (prevPage < 0) {
+          prevPage = state.nbPages - 1
+        }
+        return { ...state, page: prevPage }
+      }
+    //
     default:
       throw new Error(`no mathching '${action.type}' action type`)
   }

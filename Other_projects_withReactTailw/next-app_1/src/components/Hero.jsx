2eb0fc48ea93bd2ffea7b1a5e1a0a5c17d001a29
tomix421q <zilka.tomas421@gmail.com>
2023-02-21 21:18:08 +0100

    streda

--- Other_projects_withReactTailw/next-app_1/src/components/Hero.jsx
+++ Other_projects_withReactTailw/next-app_1/src/components/Hero.jsx
@@ -8,7 +8,11 @@ const Hero = ({ heading, message }) => {
       <div className='p-5 text-white z-[2] mt-[-10rem]'>
         <h2 className='text-5xl font-bold'>{heading}</h2>
         <p className='py-5 text-xl'>{message}</p>
-        <button className='px-8 py-2 border'>Book</button>
+        <button>
+          <a className='px-8 py-2 border' href='https://www.google.com/'>
+            Book
+          </a>
+        </button>
       </div>
     </div>
   )
